796d2d158c972311a0ec9995762211ecf9c2243a
alihasan6 <AliHasanJMM@gmail.com>
2024-06-22 14:00:56 +0000

    Added Submit Function

--- server/server.go
+++ server/server.go
@@ -3,6 +3,48 @@ package server
 
 
 
+func Submit(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/ascii-art" {
+		renderErrorPage(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "POST" {
+
+		renderErrorPage(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		renderErrorPage(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	text := r.Form.Get("text")
+
+	format := r.Form.Get("format")
+	output, err := asciiart.Ascii(text, format)
+	if err != nil {
+		renderErrorPage(w, "Internal Server", http.StatusInternalServerError)
+		return
+	}
+
+	data := PageData{
+		OutputText: output,
+	}
+
+	tmpl, err := template.ParseFiles("./statics/index.html")
+	if err != nil {
+		renderErrorPage(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		renderErrorPage(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
 func renderErrorPage(w http.ResponseWriter, errMsg string, statusCode int) {
 	w.WriteHeader(statusCode)
 	data := PageData{
