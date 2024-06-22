b42a4fd10b1cb451de47274ed53d173da819d159
alihasan6 <AliHasanJMM@gmail.com>
2024-06-22 13:58:08 +0000

    Added ASCII Function

---
+++ ascii-art/function.go
@@ -0,0 +1,38 @@
+package asciiart
+
+import (
+	"errors"
+	"os"
+	"strings"
+)
+
+func Ascii(txt, format string) (string, error) {
+	str := ""
+	txt = strings.ReplaceAll(txt, "\r\n", "\n")
+	textSlice := strings.Split(txt, "\n")
+	if !charValidation(txt) {
+		return "", errors.New("error : invalid char")
+	}
+
+	file, err := os.ReadFile("ascii-art/banner/" + format + ".txt")
+	if err != nil {
+		return "", errors.New("error : reading file")
+	}
+	slice := strings.Split(string(file), "\n")
+
+	for j, txt := range textSlice {
+		if txt != "" {
+			for i := 0; i < 8; i++ {
+				for _, v := range txt {
+					firstLine := int(v-32)*9 + 1 + i
+					str += slice[firstLine]
+				}
+				str += "\n"
+			}
+		} else if j != len(textSlice)-1 {
+			str += "\n"
+		}
+	}
+	return str, nil
+}
+
